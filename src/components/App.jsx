22dfd576d5fb929d024b7cb5624613578cc63e37
anastasiialukianenko <121677643+anastasiialukianenko@users.noreply.github.com>
2023-07-30 22:26:03 +0200

    updated code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,97 +1,33 @@
 import React, { Component } from "react";
-import api from './api';
-
 
 
 import { Searchbar } from './Searchbar/Searchbar'
 import { ImageGallery } from './ImageGallery/ImageGallery'
-import { Error } from './Error/Error';
-import { GalleryApp, LoadMoreBtn } from "./Emotion.styled";
-import { Loader } from './Loader/Loader';
+import { GalleryApp } from "./Emotion.styled";
+
 
 
 export class App extends Component {
   state = {
     imageName: '',
-    isLoading: false,
-    isError: false,
-    error: null,
-    currentPage: 1,
-    data: [],
-    isSearchSubmitted: false,
   };
-
-
-async fetchImages() {
-    const { imageName, currentPage } = this.state;
-
-    this.setState({ isLoading: true, isSearchSubmitted: true });
-
-    try {
-      const images = await api.fetchImages(imageName, currentPage);
-      this.setState((prevState) => ({
-        data: prevState.currentPage === 1 ? images : [...prevState.data, ...images],
-      }));
-    } catch (error) {
-      this.setState({ isError: true, error })
-    } finally {
-        this.setState({
-        isLoading: false
-      })}
-    }
-  
-
-  
-  
-  async componentDidUpdate(prevProps, prevState) {
-    const { imageName } = this.state;
-
-    if (imageName !== prevState.imageName) {
-      this.fetchImages();
-    }
-  }
 
 handleSubmit = (name) => {
     if (name.trim() !== '') {
-      this.setState({ imageName: name, data: [], currentPage: 1 });
+      this.setState({
+        imageName: name,
+      });
     }
   };
 
-  handleLoadMore = () => {
-
-  this.setState(
-    (prevState) => ({ currentPage: prevState.currentPage + 1 }),
-    () => {this.fetchImages(); 
-    }
-  );
-};
-
-  
   render() {
-    const { isLoading, isError, data, error, isSearchSubmitted,  } = this.state;
-    const hasImages = data.length > 0;
 
      return (
-    <GalleryApp>
-      {isError && <Error><p>{error}</p></Error>}
-      
-      <Searchbar onSubmit={this.handleSubmit} />
-      {isLoading ? (
-        <Loader />
-      ) : hasImages ? (
-        <ImageGallery imageData={data} />
-      ) : (
-        isSearchSubmitted && <Error><p>There are no pictures with this search term.</p></Error>
-      )}
-      {hasImages && <LoadMoreBtn type='button' onClick={this.handleLoadMore}>Load more</LoadMoreBtn>}
+       <GalleryApp>
+         <Searchbar onSubmit={this.handleSubmit} />
+         <ImageGallery imageName={this.state.imageName}/>
+
     </GalleryApp>
   );
-
-
-
-   
   }
 }
-
-
-
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,28 +1,93 @@
-import React from "react";
+import React, { Component } from "react";
+import api from '../api';
+import { Loader } from '../Loader/Loader';
+import { Error } from '../Error/Error';
 
 
 import PropTypes from 'prop-types'; 
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
-import { ImageGalleryList, } from "../Emotion.styled";
+import { ImageGalleryList, LoadMoreBtn } from "../Emotion.styled";
 
+export class ImageGallery extends Component {
+  static propTypes = {
+     imageName: PropTypes.string.isRequired,
+      };
+  state = {
+    status: 'idle',
+    error: null,
+    currentPage: 1,
+    data: [],
+  }
+  
+  
+  async componentDidUpdate(prevProps, prevState) {
+    
+    const { imageName } = this.props;
+    const { currentPage, } = this.state;
 
+    if (imageName !== prevProps.imageName || currentPage !== prevState.currentPage) {
+      if (imageName !== prevProps.imageName) {
+        this.setState({
+        data: [],
+        currentPage: 1,
+        status: 'pending'
+      });
+      }
 
-export const ImageGallery = ({ imageData }) => {
-    return (
-         <ImageGalleryList>
-      {imageData.map((image) => (
-        <ImageGalleryItem
-          key={image.id} 
-          webformatURL={image.webformatURL}
-          largeImageURL={image.largeImageURL}
-          tags={image.tags}
-        />
-      ))}
-    </ImageGalleryList>
-    )
+      this.fetchImages()
+    }
+  }
+
+async fetchImages() {
+    const { imageName } = this.props;
+    const { currentPage, } = this.state;
+
+    try {
+      const images = await api.fetchImages(imageName, currentPage);
+      this.setState((prevState) => ({data: currentPage === 1 ? images : [...prevState.data, ...images], status: 'resolved'}));
+    } catch (error) {
+      this.setState({ status: 'rejected', error })
+    } 
+  }
+
+  handleLoadMore = () => {
+  this.setState((prevState) => ({ currentPage: prevState.currentPage + 1 }));
 }
 
-ImageGallery.propTypes = {
-    imageData: PropTypes.array,
-}
 
+
+  render() {
+    const { status, error } = this.state;  
+    
+    if (status === 'pending') {
+     return <Loader />
+   }
+    if (status === 'rejected') {
+     return <Error><p>{error}</p></Error>
+   }
+    if (status === 'resolved') {
+if (this.state.data.length === 0) {
+      return <Error><p>There are no pictures with this search term.</p></Error>;
+    }
+
+      return (
+
+        <ImageGalleryList>
+          {this.state.data.map((image) => (
+            <ImageGalleryItem
+              key={image.id}
+              webformatURL={image.webformatURL}
+              largeImageURL={image.largeImageURL}
+              tags={image.tags}
+            />
+          ))}
+          
+          <LoadMoreBtn type='button' onClick={this.handleLoadMore}>Load more</LoadMoreBtn>
+        
+        </ImageGalleryList>
+      )
+    }
+  
+    }
+  
+}
